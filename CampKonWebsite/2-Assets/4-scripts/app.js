fa579325829bc6a05b4aaed14f26c41ceb50b4d1
Hossein <hosseinnoruzpur@outlook.com>
2024-04-14 09:41:57 +0330

    camp-6

--- CampKonWebsite/2-Assets/4-scripts/app.js
+++ CampKonWebsite/2-Assets/4-scripts/app.js
@@ -1,11 +1,19 @@
 const navBtn = document.querySelector(".nav-btn");
+const navMenu = document.querySelector(".nav-menu");
+const navMenuCloseBtn = document.querySelector(".nav-menu__top-section-closebtn");
 let navOpen = false;
 navBtn.addEventListener("click", function () {
     if (navOpen) {
         navBtn.classList.remove("nav-btn--open");
+        navMenu.classList.remove("nav-menu--open");
         navOpen = false;
     } else {
         navBtn.classList.add("nav-btn--open");
+        navMenu.classList.add("nav-menu--open");
         navOpen = true;
     }
+})
+navMenuCloseBtn.addEventListener("click" , function() {
+    navMenu.classList.remove("nav-menu--open");
+    navBtn.classList.remove("nav-btn--open");
 })
